427e08554d3dbaf350efa7e0a7eb87b3a9be8b8a
Monica Cornescu <cornescu.monica@gmail.com>
2020-02-09 21:24:33 +0200

    refactoring + new ideas

--- vehicles4/vehicles/vehicles.py
+++ vehicles4/vehicles/vehicles.py
@@ -1,56 +1,87 @@
-class Vehicle():
-  def __init__(self, wheels, max_weight, max_volume):
-    self.wheels = wheels
-    self.speed = 0
-    self._packages = [] 
-    self.max_weight = max_weight
-    self.max_volume = max_volume
+class Vehicle:
+    def __init__(self, wheels, max_weight, max_volume):
+        self.wheels = wheels
+        self.speed = 0
+        self._packages = []
+        self.max_weight = max_weight
+        self.max_volume = max_volume
 
-  def accelerate(self, amount):
-    self.speed += amount
+        # initialized to -1 (as a marker to indicate value is not calculated)
+        self._total_package_weight = -1
+        self._total_package_volume = -1
 
-  def add_packages(self, packages):
-    if not isinstance(packages, list):
-      packages = [packages]
+    def accelerate(self, amount):
+        self.speed += amount
 
-    for p in packages:
-      if self.max_weight >= p.weight + self.get_total_weight() and self.max_volume >= p.volume + self.get_total_volume():
-          self._packages.append(p)
+    def add_packages(self, packages):
+        if not isinstance(packages, list):
+            packages = [packages]
 
-    
-  # remove only one package
-  def remove_package(self, packages):
-    if not isinstance(packages, list):
-      packages = [packages]
-    for p in packages:
-      if p in self._packages:
-        self._packages.remove(p)
+        for p in packages:
+            self.add_package(p)
 
-  def get_package_count(self):
-    return len(self._packages)
+    def add_package(self, package):
+        if self._package_can_be_added(package):
+            self._packages.append(package)
+            # reset to -1 (should be recalculated if needed)
+            self._total_package_weight = -1
+            self._total_package_volume = -1
 
-  def get_total_weight(self):
-    return self._sum_packages_weight(self._packages)
+    def _package_can_be_added(self, package):
+        return self._adding_package_will_not_exceed_max_weight(package) \
+               and self._adding_package_will_not_exceed_max_volume(package)
 
-  def get_total_volume(self):
-    return self._sum_packages_volume(self._packages)
+    def _adding_package_will_not_exceed_max_weight(self, package):
+        return self.max_weight >= package.weight + self.get_total_weight()
 
-  # TODO we can do better
-  def _sum_packages_weight(self, packages):
-    ret = 0
-    return sum([p.weight for p in packages])
+    def _adding_package_will_not_exceed_max_volume(self, package):
+        return self.max_volume >= package.volume + self.get_total_volume()
 
-  def _sum_packages_volume(self, packages):
-    ret = 0
-    return sum([p.volume for p in packages])
+    # remove only one package
+    def remove_packages(self, packages):
+        if not isinstance(packages, list):
+            packages = [packages]
 
-  # TODO 
-  def get_packages(self):
-    return self._packages
+        for p in packages:
+            self.remove_package(p)
+
+    def remove_package(self, package):
+        if package in self._packages:
+            self._packages.remove(package)
+            # reset to -1 (should be recalculated if needed)
+            self._total_package_weight = -1
+            self._total_package_volume = -1
+
+    def get_package_count(self):
+        return len(self._packages)
+
+    def get_total_weight(self):
+        if self._total_package_weight == -1:
+            self._total_package_weight = self._sum_packages_weight(self._packages)
+        return self._total_package_weight
+
+    def get_total_volume(self):
+        """returns sum of all package volumes"""
+        if self._total_package_volume == -1:
+            self._total_package_volume = self._sum_packages_volume(self._packages)
+        return self._total_package_volume
+
+    # TODO we can do better
+    @staticmethod
+    def _sum_packages_weight(packages):
+        return sum([p.weight for p in packages])
+
+    @staticmethod
+    def _sum_packages_volume(packages):
+        return sum([p.volume for p in packages])
+
+    # TODO
+    def get_packages(self):
+        return self._packages
+
 
 # return the packages which did not fit
 
 
 class Car(Vehicle):
-  pass
-
+    pass
